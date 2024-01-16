de4512fe26b7dd8c515a950440617a2f52d81172
isidro <isidrohonrubia784@gmail.com>
2024-01-16 10:40:51 +0100

    otro commit para asegurar que este funcione desde ubuntu

--- pruebas/prueba.java
+++ pruebas/prueba.java
@@ -1,6 +1,6 @@
 public class prueba {
 
      public static void main(String[] args) {
-        System.out.println("otra prueba desde ubuntu");
+        System.out.println("otra prueba más desde ubuntu con cariño :3");
      }
 }
